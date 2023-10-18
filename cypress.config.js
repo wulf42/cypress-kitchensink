4b9dbaa22760f463c82def3085037d4c989666f7
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-10-18 21:10:47 +0200

    my first test, added first variable

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,9 @@ module.exports = {
   'projectId': '4b7344',
   e2e: {
     "baseUrl": "http://localhost:8080",
+    "env": {
+      "navbarText": "cypress.io"
+    }
   },
 }
 
--- cypress/e2e/2-advanced-examples/myFirstTest.cy.js
+++ cypress/e2e/2-advanced-examples/myFirstTest.cy.js
@@ -1,3 +1,5 @@
+const navbarText = Cypress.env('navbarText');
+
 context('My First Test', () => {
     beforeEach(() => {
         cy.visit('/commands/actions')
@@ -26,7 +28,7 @@ context('My First Test', () => {
     })
 
     it('correctly renders the cypress website link',() => { 
-    cy.findByText('cypress.io').should('exist')  
+    cy.findByText(navbarText).should('exist')  
     })
 })
         
