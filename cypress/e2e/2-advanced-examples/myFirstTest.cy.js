28f68f45777f73b0bbc399e2c0e0059b9c292388
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-10-18 17:26:58 +0200

    my first test, if h1 contain text, if there is p under the h1 in container

--- cypress/e2e/2-advanced-examples/myFirstTest.cy.js
+++ cypress/e2e/2-advanced-examples/myFirstTest.cy.js
@@ -6,9 +6,13 @@ context('My First Test', () => {
     it('has an h1 on the page', () => {
         cy.get('h1').should('exist');
     })
+    it('renders the correct h1 text', () => {
+        cy.get('h1').should('contain.text', 'Actions')
+    })
 
-
-
+    it('renders a paragraph under the h1', () => {
+        cy.get('.container').find('p').should('exist');
+    })
 
 
 
