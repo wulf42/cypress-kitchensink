41c2b0cbff29d8ceb465304dfd24731ab210a287
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-10-20 14:25:23 +0200

    using .then() to run non-async js commmand

--- cypress/e2e/2-advanced-examples/myFirstTest.cy.js
+++ cypress/e2e/2-advanced-examples/myFirstTest.cy.js
@@ -1,34 +1,50 @@
 const navbarText = Cypress.env('navbarText');
 
 context('My First Test', () => {
-    beforeEach(() => {
+    // beforeEach(() => {
+    //     cy.visit('/commands/actions')
+    // })
+
+    // it('has an h1 on the page', () => {
+    //     cy.get('h1').should('exist');
+    // })
+    // it('renders the correct h1 text', () => {
+    //     cy.get('h1').should('contain.text', 'Actions')
+    // })
+
+    // it('renders a paragraph under the h1', () => {
+    //     cy.get('.container').find('p').should('exist');
+    // })
+
+    // it('renders a paragraph under the h1 in specific conteiner', () => {
+    //     cy.get('.container').eq(1).find('p').should('exist');
+    // })
+
+    // it('renders a section with the correct elements', () => {
+    //     cy.get('.container').eq(2).within(()=>{
+    //         cy.get('h4').should('exist');
+    //         cy.get('p').should('exist');
+    //     })
+    // })
+
+    // it('correctly renders the cypress website link',() => { 
+    // cy.findByText(navbarText).should('exist')  
+    // })
+
+    it('types into an email field',()=>{
         cy.visit('/commands/actions')
-    })
-
-    it('has an h1 on the page', () => {
-        cy.get('h1').should('exist');
-    })
-    it('renders the correct h1 text', () => {
-        cy.get('h1').should('contain.text', 'Actions')
-    })
-
-    it('renders a paragraph under the h1', () => {
-        cy.get('.container').find('p').should('exist');
-    })
-
-    it('renders a paragraph under the h1 in specific conteiner', () => {
-        cy.get('.container').eq(1).find('p').should('exist');
-    })
-
-    it('renders a section with the correct elements', () => {
-        cy.get('.container').eq(2).within(()=>{
-            cy.get('h4').should('exist');
-            cy.get('p').should('exist');
+        cy.findByPlaceholderText('Email').type('testemail@mail.com')
+        cy.wait(5000).then(()=>{
+            console.log("test is finished")
         })
+  
+
     })
 
-    it('correctly renders the cypress website link',() => { 
-    cy.findByText(navbarText).should('exist')  
-    })
+
+
+
+
+
 })
         
